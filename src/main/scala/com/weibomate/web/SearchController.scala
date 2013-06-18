a9274e7526fbcd3da5beeaa0c2e37d24c4d4f736
Ming Jin <skyairmj@gmail.com>
2013-06-18 15:52:23 +0800

    specify charset and use base64 encoding explicitly for appfog's encoding issue

--- src/main/scala/com/weibomate/web/SearchController.scala
+++ src/main/scala/com/weibomate/web/SearchController.scala
@@ -1,6 +1,5 @@
 package com.weibomate.web
 
-import java.net.URLEncoder.encode
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.PathVariable
+import org.apache.commons.codec.binary.Base64
 
 @Controller
 @RequestMapping(Array("/search"))
@@ -18,13 +18,14 @@ class SearchController {
 
   @RequestMapping(method = Array(RequestMethod.POST))
   def search(@RequestParam("keyword") keyword: String) = {
-    "redirect:search/%s".format(encode(keyword, "utf-8"))
+    "redirect:search/%s".format(Base64.encodeBase64URLSafeString(keyword.getBytes("UTF-8")))
   }
 
   @RequestMapping(value=Array("/{keyword}"), method = Array(RequestMethod.GET))
   def show(@PathVariable("keyword") keyword: String, modelMap: ModelMap) = {
-    logger.info("------------get search keyword: %s.-----------".format(keyword))
-    val tweets = searchService.search(keyword)
+    val keyword2 = new String(Base64.decodeBase64(keyword), "UTF-8")
+    logger.info("------------get search keyword: %s.-----------".format(keyword2))
+    val tweets = searchService.search(keyword2)
     tweets.foreach((doc) => logger.info("---Weibo: id: %s; text: %s; picture: %s".format(doc.getId(), doc.getText(), doc.getThumbnailPic())))
     modelMap.addObject("tweets", tweets)
     "search"
